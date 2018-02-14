6e58e08aae5f1c70d5a7991b7ebd11142ff26d0b
darthspectrum <muradianator@gmail.com>
2018-02-14 18:05:22 +0400

    Drafted REST with separate routers for user, token and prisms

--- app.js
+++ app.js
@@ -10,28 +10,63 @@ var prismDB;
 const app = express();
 app.use(bodyParser.json());
 
-app.route('/prism')
-  .get(function(req, response) {
-    console.log('Fetching all prisms for ' + req.query.URLs);
-    const crit = JSON.parse(req.query.URLs);
+// User router
+const userRouter = express.Router({ mergeParams: true });
 
-    if (Array.isArray(crit)) {
-      prismDB.collection('prisms').find({ url: { $in: crit } }).toArray(
-        function(err, results) {
+userRouter.route('/users')
+  .post(function (req, response) {
+    const userSpec = req.body;
+    console.log('Registering a user');
+    console.log(userSpec);
+    response.send('Registration success!');
+  });
+
+userRouter.route('/users/:userId')
+  .get(function (req, response) {
+    const userSpec = req.body;
+    console.log('Fetching a user with ID : ' + req.params.userId);
+    console.log(userSpec);
+    response.send('Fetching a user with ID : ' + req.params.userId);
+  });
+
+userRouter.route('/users/:userId')
+  .delete(function (req, response) {
+    const userSpec = req.body;
+    console.log('Deleting user with ID : ' + req.params.userId);
+    console.log(userSpec);
+    response.send('Removed user with ID : ' + req.params.userId);
+  });
+
+// Prism router
+const prismRouter = express.Router({ mergeParams: true });
+
+prismRouter.route('/prisms')
+  .get(function (req, response) {
+    const URLs = null;
+    if(req.query.URLs)
+       URLs = JSON.parse(req.query.URLs);
+
+    if (Array.isArray(URLs)) {
+      console.log('Fetching prisms for user ' + req.params.userId + ' for URLs ' + req.query.URLs);
+      prismDB.collection('prisms').find({ url: { $in: URLs } }).toArray(
+        function (err, results) {
           console.log(results);
           console.log(err);
           response.send(results);
         });
     } else {
-      prismDB.collection('prisms').find({ url: crit }).toArray(
-        function(err, results) {
+      console.log('Fetching all prisms for user ' + req.params.userId);
+      prismDB.collection('prisms').find({ url: URLs }).toArray(
+        function (err, results) {
           console.log(results);
           console.log(err);
           response.send(results);
         });
     }
-  })
-  .put(function(req, response) {
+  });
+
+prismRouter.route('/prisms')
+  .put(function (req, response) {
     const prism = req.body;
     console.log(prism);
     prismDB.collection('prisms')
@@ -48,32 +83,48 @@ app.route('/prism')
       }, { upsert: true });
   });
 
+prismRouter.route('/prisms/:prismId')
+  .delete(function (req, response) {
+    console.log('deleting prism with ID: ' + req.params.prismId);
+  });
+
+prismRouter.route('/prisms')
+  .delete(function (req, response) {
+    console.log('deleting prism all prisms for user : ' + req.params.userId);
+  });
+
+// Token router
+const tokenRouter = express.Router({ mergeParams: true });
+
+tokenRouter.route('/tokens')
+  .post(function (req, response) {
+    const credentials = req.body;
+    console.log('login');
+    console.log(credentials);
+    response.send('login success');
+  });
+
 // HTTPS server
 const privateKey = fs.readFileSync('key.pem', 'utf8');
 const certificate = fs.readFileSync('cert.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(11111, function() {
+httpsServer.listen(11111, function () {
   console.log('Started Prism HTTPS server on 11111');
 });
+
+userRouter.use('/users/:userId', prismRouter);
+app.use('', userRouter);
+app.use('', tokenRouter);
 
 //MongoDB
 const mongoURL = 'mongodb://localhost:27017/prism';
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(mongoURL, function(err, db) {
+MongoClient.connect(mongoURL, function (err, db) {
   if (err === null) {
     console.log('Connected to MongoDB...');
     prismDB = db;
   }
 });
-
-// Used for tests via Postman, since I can't find a
-// way to import the SSL certificate into it/chrome
-// var http = require('http');
-// const httpServer = http.createServer(app);
-
-// httpServer.listen(1111, function() {
-//   console.log('Started Prism HTTP server on 1111');
-// });
